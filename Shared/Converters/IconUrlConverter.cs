9d9d27bf40cf1bca0efe1d737a414d84d80ded3a
Konstantinos Koumpanakis <kko@entersoft.gr>
2025-12-28 14:59:08 +0200

    ui converters

---
+++ Shared/Converters/IconUrlConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp;
+
+public class IconUrlConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not string || value is null) return "";
+
+        var iconCode = value as string;
+        return iconCode != null
+            ? $"https://openweathermap.org/img/wn/{iconCode}@2x.png"
+            : "";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
+
---
+++ Shared/Converters/UnixToDateConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp;
+
+public class UnixToDateConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is long unix)
+            return DateTimeOffset.FromUnixTimeSeconds(unix).DateTime;
+
+        return DateTime.Now;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
