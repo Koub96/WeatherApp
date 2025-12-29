e0bacab8542f92c2bef7ea8c5143bf64628830d0
Konstantinos Koumpanakis <kko@entersoft.gr>
2025-12-29 17:31:59 +0200

    added converter to negate bool binded value

---
+++ Shared/Converters/NegateBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp;
+
+public class NegateBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not bool || value is null) return false;
+
+        return !((bool)value);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
+
